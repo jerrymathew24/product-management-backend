794cbbaf531999b22897b159fcaa5cc57353e721
PUMKIN24 <jer2ymatheww@gmail.com>
2024-02-25 00:43:36 +0530

    protecting routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,13 @@ const router = express.Router()
 //routes
 //register || POST
 router.post('/register', registerUser)
+//login || POST
 router.post('/login', loginUser)
+//test || GET
 router.get('/test', protect, testController)
+//protected || GET
+router.get('/user-auth', protect, (req, res) => {
+    res.status(200).send({ ok: true })
+})
 
 export default router   
